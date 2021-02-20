8a3b03b5a10c33c2b8bc77f95ccb36e2c19b01dc
vasilisdimitriou <vasilisdmr@gmail.com>
2021-02-20 19:53:49 +0200

    Created AccountController which will be used to expose the endpoint for the account creation.

---
+++ CustomerAPI/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CustomerAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/customers/{customerId}/openaccount")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountRepository _accountRepository;
+        private readonly ITransactionRepository _transactionRepository;
+        private readonly ICustomerRepository _customerRepository;
+
+        public AccountController(IAccountRepository accountRepository,
+            ITransactionRepository transactionRepository,
+            ICustomerRepository customerRepository)
+        {
+            _accountRepository = accountRepository ??
+                throw new ArgumentNullException(nameof(accountRepository));
+
+            _transactionRepository = transactionRepository ??
+                throw new ArgumentNullException(nameof(transactionRepository));
+
+            _customerRepository = customerRepository ??
+                throw new ArgumentNullException(nameof(customerRepository));
+        }
+
+
+       [HttpPost("{initialCredit}")]
+       public ActionResult OpenAccountForCustomer(Guid customerId,int initialCredit)
+        {
+            if (!_customerRepository.CustomerExists(customerId))
+            {
+                return NotFound();
+            }
+
+            var customerFromRepo = _customerRepository.GetCustomer(customerId);
+
+            return Ok();
+        }
+    }
+}
