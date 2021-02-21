5eed5dd096318e3dc7f52687b8709681a1edabeb
vasilisdimitriou <vasilisdmr@gmail.com>
2021-02-21 13:24:35 +0200

    Created profiles folder and AccountProfile to be able to use mapper to map from Account entity to AccountDTO and back.

---
+++ CustomerAPI/Profiles/AccountProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AutoMapper;
+using CustomerAPI.Entities;
+using CustomerAPI.Models;
+
+namespace CustomerAPI.Profiles
+{
+    public class AccountProfile : Profile
+    {
+        public AccountProfile()
+        {
+            CreateMap<Account, AccountDTO>();
+            CreateMap<AccountDTO, Account>();
+        }
+    }
+}
