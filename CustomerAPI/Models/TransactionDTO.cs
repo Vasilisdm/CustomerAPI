0b5a1c48ac90cf85d3149505bfdc6e12914f1206
vasilisdimitriou <vasilisdmr@gmail.com>
2021-02-21 17:16:30 +0200

    Created TransactionDTO

---
+++ CustomerAPI/Models/TransactionDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CustomerAPI.Models
+{
+    public class TransactionDTO
+    {
+        public decimal Amount { get; set; }
+
+        public Guid AccountId { get; set; }
+    }
+}
