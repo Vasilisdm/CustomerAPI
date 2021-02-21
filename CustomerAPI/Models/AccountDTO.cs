efab875a5f0e4264ca04a7cc6bac662d67b60d44
vasilisdimitriou <vasilisdmr@gmail.com>
2021-02-21 13:25:08 +0200

    Created accountDTO model which is going to be used as the returned type of the newly created account.

---
+++ CustomerAPI/Models/AccountDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CustomerAPI.Models
+{
+    public class AccountDTO
+    {
+        public Guid Id { get; set; }
+
+        public Guid CustomerId { get; set; }
+
+        public decimal Balance { get; set; }
+
+    }
+}
