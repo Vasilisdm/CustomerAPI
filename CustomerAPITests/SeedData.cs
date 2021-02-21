36e95ba99ca8a03acae4d0e6e3b5b046c7d4e1b4
vasilisdimitriou <vasilisdmr@gmail.com>
2021-02-21 23:03:56 +0200

    Created a simple static class which will be used to seed the inmemory database with a customer.

---
+++ CustomerAPITests/SeedData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CustomerAPI;
+using CustomerAPI.Entities;
+
+namespace CustomerAPITests
+{
+    public class SeedData
+    {
+        public static void PopulateTestData(AccountContext accountContext)
+        {
+            accountContext.Customers.Add(new Customer
+            {
+                Id = Guid.Parse("d20b0d04-d371-48b7-99aa-0e2ac74ff98e"),
+                FirstName = "Vassilis",
+                LastName = "Dimitriou"
+            });
+        }        
+    }
+}
